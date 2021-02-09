d78ae38dc362ad6e0fb8d60dc3a4532d1a41f758
Kamila <kamila.dynysiuk@gmail.com>
2021-02-09 21:56:20 +0000

    modyfied archive files

--- archive-beginner.php
+++ archive-beginner.php
@@ -10,27 +10,21 @@
 
 get_header(); ?>
 
+<?php fte_banner_header(); ?>
 
 <div id="primary" class="content-area">
     <main id="main" class="site-main" role="main">
-        <?php if (current_user_can('read_group_beginner')) : ?>
+        <?php if (current_user_can('read_group_beginner')) :
 
-            <div class="banner-group">
-                <h1>Grupa: Beginner</h1>
-                <?php if (is_user_logged_in()) :
-                    echo '<h2 class="member-name"> Hello ' . $current_user->user_login . '!!! </h2>';
-                endif; ?>
-            </div>
 
-        <?php
             get_template_part('template-parts/content-group', get_post_format());
 
         else : ?>
             <div class="not-logged">
                 <h1>Strefa studenta</h1>
                 <h2>Grupa Beginner</h2>
-                <p> Wygląda na to, że nie jesteś w tej grupie, brak dostępu.
-                    Jeśli jesteś uczestnikiem kursu wybierz swoją grupę:</p>
+                <p>Jeśli jesteś uczestnikiem grupy beginner <a class="login" href="<?php echo  wp_login_url() ?>">zaloguj się</a></p>
+                <p> Lub wybierz swoją grupę:</p>
                 <ul>
                     <li><a href="<?php echo esc_url(site_url('/student/intermediate')); ?>" style="color: #01c6b8;">Intermediate</a></li>
                     <li><a href="<?php echo esc_url(site_url('/student/advanced')); ?>" style="color: #01c6b8;">Advanced</a></li>
