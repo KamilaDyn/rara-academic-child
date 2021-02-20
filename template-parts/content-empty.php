9bebffbe29d74ae171ff3cd9f2e1f92fa6984e32
Kamila <kamila.dynysiuk@gmail.com>
2021-02-20 22:39:57 +0000

    add empty content to group

---
+++ template-parts/content-empty.php
@@ -0,0 +1,21 @@
+<?php
+
+/**
+ * Template part for displaying a message that posts cannot be found.
+ *
+ * @link https://codex.wordpress.org/Template_Hierarchy
+ *
+ * @package Rara_Academic
+ */
+
+?>
+
+<section class="no-results not-found">
+    <header class="page-header">
+        <h1 class="page-title"><?php esc_html_e('No published post', 'rara-academic'); ?></h1>
+    </header><!-- .page-header -->
+
+    <div class="page-content">
+        <p>There is nothing published yet, please wait for it, or contact with me.</p>
+    </div><!-- .page-content -->
+</section><!-- .no-results -->
